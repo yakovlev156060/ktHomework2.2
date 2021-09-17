5b9e6bbaea9f1b7a789fb681d5c8768d4a6d3064
Yakovlev Alexandr <ventry2016@gmail.com>
2021-09-17 14:47:35 +0500

    Second commit

--- src/main/kotlin/main.kt
+++ src/main/kotlin/main.kt
@@ -1,19 +1,26 @@
-fun main(){
-    val likes =10001
+fun main() {
+    val likes = 74
 
     //var rezult:Int=(amount*0.0075F).toInt()
-    var rezult:String=""
-    var remainder:Int=likes%100
+    var rezult: String = ""
+    var remainder: Int = likes % 100
 
-    if(remainder==1){
-        rezult="лайк"
-    }else {
-
-        if ((remainder >= 2) && (remainder <= 4)) {
-            rezult = "лайка"
+    if ((remainder >= 11) && (remainder <= 15)) {
+        rezult = "лайков"
+    }
+    else{
+        if (remainder % 10 == 1) {
+            rezult = "лайк"
         } else {
-            rezult = "лайков"
+
+            if ((remainder%10 >= 2) && (remainder%10 <= 4)) {
+                rezult = "лайка"
+            } else {
+                rezult = "лайков"
+            }
         }
     }
+
+
     println("$likes $rezult")
 }
